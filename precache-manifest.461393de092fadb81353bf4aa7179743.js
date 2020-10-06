fe28f302de11edec3e0e8fcea615c76415294764
Jacob Bruce <brucejac@sheridancollege.ca>
2020-10-06 18:01:45 -0400

    Updates

---
+++ precache-manifest.461393de092fadb81353bf4aa7179743.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35703a5ec5538cc6b45f77593cc1585d",
+    "url": "/ip-tracker/index.html"
+  },
+  {
+    "revision": "8c7caff98505d271560d",
+    "url": "/ip-tracker/static/css/main.20ef6a1f.chunk.css"
+  },
+  {
+    "revision": "ec4c4c6b8cddefff9125",
+    "url": "/ip-tracker/static/js/2.b327f405.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/ip-tracker/static/js/2.b327f405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c7caff98505d271560d",
+    "url": "/ip-tracker/static/js/main.ae256147.chunk.js"
+  },
+  {
+    "revision": "bac147f6a83fad4c840f",
+    "url": "/ip-tracker/static/js/runtime-main.b1593b04.js"
+  },
+  {
+    "revision": "53ef8ed8ef4b92721f00cb8bfdc5c9b9",
+    "url": "/ip-tracker/static/media/icon-arrow.53ef8ed8.svg"
+  },
+  {
+    "revision": "8d3f4ad64bf3a9320e172edf01e0c016",
+    "url": "/ip-tracker/static/media/pattern-bg.8d3f4ad6.png"
+  }
+]);
